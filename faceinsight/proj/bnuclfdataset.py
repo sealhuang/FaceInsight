41c457fdb9ff7706f64e65a3c7b432d83539f446
Lijie Huang <huanglijie@outlook.com>
2019-04-22 15:12:00 +0800

    update

--- faceinsight/proj/bnuclfdataset.py
+++ faceinsight/proj/bnuclfdataset.py
@@ -130,3 +130,78 @@ class MBTIFaceDataset(Dataset):
         fmt_str += '{0}{1}\n'.format(tmp, self.transform.__repr__().replace('\n', '\n' + ' ' * len(tmp)))
         return fmt_str
 
+class PF16FaceDataset(Dataset):
+    """16PF data loader.
+    
+    Args:
+        csv_file (string): 16PF data file.
+        img_dir (string): Directory path of face images.
+        factor_name (string): target factor name.
+        sample_num_per_end (int): select N samples from each end of the `factor`
+            value distribution.
+        class_target (boolean): True for default.
+        gender_filter (string): 'male', 'female', or None for default.
+        transform (callable, optional): A function/transform that takes in
+            a sample and returns a transformed version.
+            E.g. ``transforms.RandomCrop`` for images.
+        target_transform (calllable, optional): A function/transform that takes
+            in the target and transforms it.
+
+    """
+    def __init__(self, csv_file, img_dir,
+                 factor_name, sample_num_per_end, class_target=True,
+                 gender_filter=None, transform=None, target_transform=None):
+        # read csv info and get dataset
+        csv_info = open(csv_file).readlines()
+        csv_info = [line.strip().split(',') for line in csv_info]
+        header = csv_info.pop(0)
+        target_idx = header.index(factor_name)
+        # gender filter
+        if gender_filter:
+            if gender_filter not in ['male', 'female']:
+                raise(RuntimeError('Invalid gender_filter argument.'))
+            csv_info = [line for line in csv_info if line[1]==gender_filter]
+        samples = make_dataset(csv_info, img_dir, target_idx,
+                               sample_num_per_end=sample_num_per_end,
+                               class_target=class_target)
+        if len(samples)==0:
+            raise(RuntimeError('Found 0 files in face folder.'))
+
+        self.face_dir = os.path.expanduser(img_dir)
+        self.target_factor = factor_name
+        self.samples = samples
+        self.targets = [s[1] for s in samples]
+
+        self.transform = transform
+        self.target_transform = target_transform
+
+    def __getitem__(self, idx):
+        """
+        Args:
+            idx (int): Index
+
+        Returns:
+            tuple: (image, target) where target is factor value of MBTI factor.
+        """
+        img_path, target = self.samples[idx]
+        # load face image
+        with open(img_path, 'rb') as f:
+            img = Image.open(f).convert('RGB')
+        if self.transform is not None:
+            img = self.transform(img)
+        if self.target_transform is not None:
+            target = self.target_transform(target)
+
+        return img, target
+
+    def __len__(self):
+        return len(self.samples)
+
+    def __repr__(self):
+        fmt_str = 'Dataset ' + self.__class__.__name__ + '\n'
+        fmt_str += '    Number of datapoints: {}\n'.format(self.__len__())
+        fmt_str += '    Face dir location : {}\n'.format(self.face_dir)
+        tmp = '    Transforms (if any): '
+        fmt_str += '{0}{1}\n'.format(tmp, self.transform.__repr__().replace('\n', '\n' + ' ' * len(tmp)))
+        return fmt_str
+
