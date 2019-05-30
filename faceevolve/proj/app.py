bd34c357d63902967776065eb36a1418302fbb2d
Lijie Huang <huanglijie@outlook.com>
2019-05-30 16:36:35 +0800

    update apps

--- faceevolve/proj/app.py
+++ faceevolve/proj/app.py
@@ -4,15 +4,23 @@ import os
 from flask import Flask, flash, request, redirect, url_for, send_from_directory
 from werkzeug.utils import secure_filename
 from flask import request, render_template
-#import catdog
+from model_test import load_ensemble_model
+from model_test import crop_face
+from model_test import align_face
+from model_test import face_eval
+
 
 UPLOAD_FOLDER = os.path.expanduser('~/Downloads/uploads')
 if not os.path.exists(UPLOAD_FOLDER):
     os.makedirs(UPLOAD_FOLDER, mode=0o755)
 ALLOW_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
 
+# load model
+FACTOR = 'A'
+ensemble_model = load_ensemble_model(FACTOR)
+
 # initialize the app
-app = Flask(__name__)
+app = Flask(__name__, template_folder='./')
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 def allowed_file(filename):
@@ -38,12 +46,25 @@ def upload_file():
             return redirect(request.url)
         if f and allowed_file(f.filename):
             filename = secure_filename(f.filename)
-            f.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return redirect(url_for('uploaded_file', filename=filename))
+            saved_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            f.save(saved_path)
+            #return redirect(url_for('uploaded_file', filename=filename))
+            # eval
+            crop_face_file = crop_face(saved_path, app.config['UPLOAD_FOLDER'],
+                                       20, 1.4, 224,
+                                       detect_multiple_faces=False)
+            if crop_face_file:
+                aligned_face_file = align_face(crop_face_file, 224, 1.4)
+                if aligned_face_file:
+                    score = face_eval(aligned_face_file, ensemble_model)
+
+                    return render_template('result.html', pred=score,
+                                           imgpath=url_for('uploaded_file',
+                                                           filename=filename))
     return """
     <!doctype html>
-    <title>Upload new File</title>
-    <h1>Upload new File</h1>
+    <title>Upload Face Image</title>
+    <h1>Upload Face Image</h1>
     <form method=post enctype=multipart/form-data>
       <input type=file name=file>
       <input type=submit value=Upload>
@@ -68,4 +89,5 @@ def upload_file():
 #--------- RUN WEB APP SERVER ------------#
 # Start the app server on port 80
 # (The default website port)
-app.run(host='127.0.0.1', port=5000, debug=True)
+#app.run(host='127.0.0.1', port=5000, debug=True)
+app.run(host='192.168.0.152', port=5000, debug=True)
